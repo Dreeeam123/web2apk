4a4da1f2bd873188d295527da7de20ccbf8ff1e4
Dreeeam123 <blxydream@gmail.com>
2026-05-19 20:05:57 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,44 @@
-// Made by devilAPI with <3
-// https://github.com/devilAPI/web2apk
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
-void main() => runApp(MyApp());
 
 var appName = "GainVault";
 var url = "https://v0-ki-muskelaufbau-app.vercel.app/";
 var appColor = Colors.blue;
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-  WebViewController _controller;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appName,
-      theme: ThemeData(
-        primarySwatch: appColor,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appName),
-        ),
-        body: Center(
-          child: WebView(
-            initialUrl: url,
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller = webViewController;
-            },
-          ),
-        ),
-      ),
+      theme: ThemeData(primarySwatch: appColor),
+      home: WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(appName)),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
